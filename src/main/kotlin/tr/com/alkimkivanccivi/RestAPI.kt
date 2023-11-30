50ecf2b35aeb3a1b7287c0db8c8079a19257c668
Alkım Kıvanç Çivi <vonkez@users.noreply.github.com>
2023-11-30 22:42:32 +0300

    add display names

--- src/main/kotlin/tr/com/alkimkivanccivi/RestAPI.kt
+++ src/main/kotlin/tr/com/alkimkivanccivi/RestAPI.kt
@@ -1,28 +1,30 @@
 package tr.com.alkimkivanccivi
 
 import com.google.firebase.auth.FirebaseAuth
-import com.google.firebase.auth.FirebaseAuthException
 import io.ktor.http.*
 import io.ktor.server.application.*
 import io.ktor.server.request.*
 import io.ktor.server.response.*
 import io.ktor.server.routing.*
-import org.jetbrains.exposed.sql.Database
+import kotlinx.serialization.Serializable
+import tr.com.alkimkivanccivi.database.Message
 import tr.com.alkimkivanccivi.database.MessageService
-import java.lang.IllegalArgumentException
 
+
+@Serializable
+data class NewMessagesResult(val messages: List<Message>, val users: Map<String, String>)
 
 fun Application.addRestRoutes(messageService: MessageService) {
 
     routing {
-        get("/readNewMessagesByUser/{timestamp}") {
+        get("/newMessages/{timestamp}") {
             val timestamp = call.parameters["timestamp"]?.toLongOrNull()
             if (timestamp == null){
                 call.respond(HttpStatusCode.BadRequest)
                 return@get
             }
             val token = call.request.header("Authorization")
-
+            println("token: $token")
             if (token.isNullOrBlank()){
                 call.respond(HttpStatusCode.Unauthorized)
                 return@get
@@ -35,7 +37,17 @@ fun Application.addRestRoutes(messageService: MessageService) {
                 return@get
             }
             val messages = messageService.readNewMessagesByUser(id,timestamp)
-            call.respond(messages)
+            val userIds = messages.map { if (it.receiverId == id) it.senderId else it.receiverId  }.toSet()
+            val users =userIds.map {
+                val userRecord = FirebaseAuth.getInstance().getUser(id)
+                if (userRecord.displayName == null){
+                    return@map id to userRecord.email!!
+                } else {
+                    return@map id to userRecord.displayName
+                }
+            }.toMap()
+            val result = NewMessagesResult(messages, users)
+            call.respond(result)
 
         }
     }
